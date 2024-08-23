ce010238315d573ecac4b80b77e6052093a09154
Joop Kiefte <ikojba@gmail.com>
2024-08-23 20:01:45 +0100

    Copy over highlights from Fountain grammar

---
+++ languages/fountain/highlights.scm
@@ -0,0 +1,15 @@
+[
+ (action)
+ (dialogue)
+] @spell
+(parenthetical) @markup.italic
+
+(character) @markup.heading.2
+(transition) @keyword
+
+[
+ (section_heading)
+ (scene_heading)
+] @markup.heading.1
+
+(note) @comment
